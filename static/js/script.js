2424813ffd4047499dc86510dfbe4ed64da0bb85
Abdul Azeez Adediran <abdulazeez@Clarences-MacBook-Pro.local>
2019-08-18 19:12:53 +0800

    initial commit

---
+++ static/js/script.js
@@ -0,0 +1,104 @@
+
+// Select all links with hashes
+$('a[href*="#"]')
+	.not('[href="#"]')
+	.not('[href="#0"]')
+	.click(function(event) {
+		if (
+			location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
+			&& 
+			location.hostname == this.hostname
+		) {
+			var target = $(this.hash);
+			target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+			if (target.length) {
+				event.preventDefault();
+				$('html, body').animate({
+					scrollTop: target.offset().top
+				}, 1000, function() {
+					var $target = $(target);
+					$target.focus();
+					if ($target.is(":focus")) { 
+						return false;
+					} else {
+						$target.attr('tabindex','-1'); 
+						$target.focus(); 
+					}
+				});
+			}
+		}
+	});
+
+$(document).ready(function(){
+  $("a").on('click', function(event) {
+    if (this.hash !== "") {
+      event.preventDefault();
+
+      var hash = this.hash;
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function(){
+        window.location.hash = hash;
+      });
+    }
+  });
+});
+
+AOS.init({
+  disable: 'mobile'
+}); 
+
+AOS.init({
+  disable: function () {
+    var maxWidth = 800;
+    return window.innerWidth < maxWidth;
+  }
+});
+
+
+
+// document.onmousemove = animateCircles;
+// var colors = ['#b49882','#451e1f','#b49882','#451e1f']
+
+// function animateCircles (event) {
+    
+//     var circle = document.createElement("div");
+//     circle.setAttribute("class", "circle");
+//     // document.getElementsByTagName('section').appendChild(circle);
+//     // document.getElementById("section").appendChild(circle);
+//     document.body.appendChild(circle);
+    
+//     // $('section').append(circle);
+    
+//     circle.style.left = event.clientX + 'px';
+//     circle.style.top = event.clientY + 'px';
+    
+//     var color = colors[Math.floor(Math.random() * colors.length)];
+    
+//     circle.style.borderColor = color;
+    
+//     circle.style.transition = "all 0.5s linear 0s";
+    
+//     circle.style.left = circle.offsetLeft - 20 + 'px';
+//     circle.style.top = circle.offsetTop - 20 + 'px';
+    
+//     circle.style.width = "50px";
+//     circle.style.height = "50px";
+//     circle.style.borderWidth = "5px";
+//     circle.style.opacity = "0";
+
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ static/mail_handler.php
@@ -0,0 +1,20 @@
+<?php
+	if (isset ($_POST['SendMail'])){
+		$name=$_POST['name'];
+		$email=$_POST['email'];
+		$phone=$_POST['phone'];
+		$msg=$_POST['message'];
+
+		$to='hazeezadediran@gmail.com'; // Receiver Email ID, Replace with your email ID
+		$subject='MESSAGE FROM MY PORTFOLIO';
+		$msg="Name : ".$name."\n"."Phone :     ".$phone."\n"."Email address:   ".$email." \n"."\n"."Wrote the following : "."\n\n".$msg;
+		$headers="From: ".$email;
+
+		if(mail($to, $subject, $msg, $headers)){
+			echo "success";
+		}
+		else{
+			echo "Something went wrong!";
+		}
+	}
+?>
